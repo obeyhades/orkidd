5d3096c3cde6e4ea5a116bab0af4e7069846819f
obeyhades <abdu.nazar145@gmail.com>
2026-05-14 18:33:40 +0200

    feat: add sanity schemaTypes

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,11 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
+import { categoryType } from './categoryType'
+import { customerType } from './customerType'
+import { productType } from './productType'
+import { orderType } from './orderType'
+
+
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [categoryType, customerType, productType, orderType]
 }
